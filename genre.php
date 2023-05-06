55791256fb8e244be736c90b49c382540eebee2f
BuildTools <unconfigured@null.spigotmc.org>
2023-05-06 22:09:45 +0800

    :333

--- genre.php
+++ genre.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 include('connection.php');
-
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,8 +11,7 @@ include('connection.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Genre</title>
     <!-- LINK LATEST BOOTSTRAP -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <!-- LINK BOOTSRAP ICONS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <!-- Font Awesome -->
@@ -28,46 +27,44 @@ include('connection.php');
 
 <body>
     <form method="POST">
-    <div class="d-flex flex-row justify-content-between p-3">
-        <div class="navbarfirst">
-            <div class="input-group">
-                <div class="form-outline">
-                    <input id="search-input" type="search" id="form1" class="form-control" />
-                    <label class="form-label" for="form1">Search</label>
+        <div class="d-flex flex-row justify-content-between p-3">
+            <div class="navbarfirst">
+                <div class="input-group">
+                    <div class="form-outline">
+                        <input id="search-input" type="search" id="form1" class="form-control" />
+                        <label class="form-label" for="form1">Search</label>
+                    </div>
+                    <button id="search-button" type="button" class="btn btn-primary">
+                        <i class="fas fa-search"></i>
+                    </button>
                 </div>
-                <button id="search-button" type="button" class="btn btn-primary">
-                    <i class="fas fa-search"></i>
-                </button>
+            </div>
+
+            <div class="navbarsecond">
+                <button class="btn btn-primary"><a href="genreadd.php" style="text-decoration: none; color: white;">Add</a></button>
             </div>
         </div>
 
-        <div class="navbarsecond">
-            <a href="genreadd.php"><button class="btn btn-primary">Add</button></a>
-        </div>
-    </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">Genre</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+            </tbody>
+        </table>
 
-    <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">id</th>
-            <th scope="col">Genre</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>   
-        </tbody>
-      </table>
-
-      </form>
+    </form>
 
 
 
     <!-- LINK LATEST BOOTSTRAP JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- genreadd.php
+++ genreadd.php
@@ -1,11 +1,21 @@
 <?php
 include('connection.php');
 
-if(isset($_POST['save'])){
+if (isset($_POST['save'])) {
     $genre_name = $_POST['genre_name'];
+    $results = mysqli_query($connection, "SELECT * FROM genreTBL WHERE genre_name = '$genre_name'");
+
+
+    if(mysqli_num_rows($results) > 0){
+        echo '<script>alert("Genre Already Exists!")</script>';
+        echo '<script>window.location.href = "genreadd.php"</script>';
+    }
+
+    else{
     mysqli_query($connection, "INSERT INTO `genreTBL`(`genre_name`) VALUES ('$genre_name')");
     echo '<script>alert("Genre Saved!")</script>';
     echo '<script>window.location.href = "genre.php"</script>';
+    }
 }
 
 ?>
@@ -18,33 +28,30 @@ if(isset($_POST['save'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Genre Add</title>
     <!-- LINK LATEST BOOTSTRAP -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <!-- LINK BOOTSRAP ICONS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
 
 <body>
     <form method="POST">
-    <div class="vh-100">
-        <div class="d-flex justify-content-start p-3">
-            <a href="genre.html"><button class="btn btn-primary">Back</button></a>
-        </div>
+        <div class="vh-100">
+            <div class="d-flex justify-content-start p-3">
+                <button class="btn btn-primary"><a href="genre.php" style="text-decoration: none; color: white;">Back</a></button>
+            </div>
 
-        <div class="d-flex justify-content-center align-items-center" style="height: 100%; padding-bottom: 150px !important;">
-            <div class="row row-cols-1 gy-3 gx-0" style="width: 300px;">
-                <label for="">Genre Name</label>
-                <input type="text" name="genre_name" id="" required>
-                <button class="btn btn-success" name="save">Save</button>
+            <div class="d-flex justify-content-center align-items-center" style="height: 100%; padding-bottom: 150px !important;">
+                <div class="row row-cols-1 gy-3 gx-0" style="width: 300px;">
+                    <label for="">Genre Name</label>
+                    <input type="text" name="genre_name" id="">
+                    <button class="btn btn-success" name="save">Save</button>
+                </div>
             </div>
         </div>
-    </div>
 
     </form>
     <!-- LINK LATEST BOOTSTRAP JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
